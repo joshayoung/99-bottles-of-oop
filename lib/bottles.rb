a45cecef6c0ec8ed7057af5f46c485be04e37967
Josh Young <j22cal@gmail.com>
2019-05-05 12:23:49 -0500

    Initial Exercise: first three tests passing

--- lib/bottles.rb
+++ lib/bottles.rb
@@ -1,8 +1,16 @@
 class Bottles
   def verse(bottles)
-    <<-VERSE
+    case
+    when bottles < 3
+    return <<-VERSE
+#{bottles} bottles of beer on the wall, #{bottles} bottles of beer.
+Take one down and pass it around, #{bottles - 1} bottle of beer on the wall.
+VERSE
+    else
+    return <<-VERSE
 #{bottles} bottles of beer on the wall, #{bottles} bottles of beer.
 Take one down and pass it around, #{bottles - 1} bottles of beer on the wall.
 VERSE
+    end
   end
 end
