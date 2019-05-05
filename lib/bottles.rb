1dc4505ece7900e53e39ab96c112142cbdf864f1
Josh Young <j22cal@gmail.com>
2019-05-05 12:18:00 -0500

    Initial Exercise: first two tests passing

--- lib/bottles.rb
+++ lib/bottles.rb
@@ -1,8 +1,8 @@
 class Bottles
-  def verse(arg)
+  def verse(bottles)
     <<-VERSE
-99 bottles of beer on the wall, 99 bottles of beer.
-Take one down and pass it around, 98 bottles of beer on the wall.
+#{bottles} bottles of beer on the wall, #{bottles} bottles of beer.
+Take one down and pass it around, #{bottles - 1} bottles of beer on the wall.
 VERSE
   end
 end
