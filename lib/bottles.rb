f8c05666b491117d58f88f6cbfe146638491ec1f
Josh Young <j22cal@gmail.com>
2019-06-02 13:58:20 -0500

    Code cleanup

--- lib/bottles.rb
+++ lib/bottles.rb
@@ -1,28 +1,10 @@
 class Bottles
   def verse(bottles)
     "#{no_more_line_one(bottles)}#{container(bottles)} of beer on the wall, #{no_more_line_one(bottles, true)}#{container(bottles)} of beer.\n" +
-    "#{second_phrase_one(bottles)} #{no_more(bottles)}#{container(bottles - 1, true)} of beer on the wall.\n"
+    "#{phrase(bottles)} #{no_more(bottles)}#{container(bottles - 1, true)} of beer on the wall.\n"
   end
 
-  def container(quantity, second_line = false)
-    return quantity.to_s + " bottle" if quantity == 1
-    return "bottles" if quantity == -1
-    return "bottles" if quantity == 0
-    return "1 six-pack" if quantity == 6 && !second_line
-    quantity.to_s + " bottles"
-  end
-
-  def second_phrase_one(bottles)
-    return "Go to the store and buy some more," if bottles == 0
-    "Take #{pronoun(bottles)} down and pass it around,"
-  end
-
-  def pronoun(bottles)
-    return "it" if bottles == 1
-    "one"
-  end
-
-  def no_more(bottles)
+  def no_more(bottles, lower = false)
     return "no more " if bottles == 1
     "99 " if bottles == 0
   end
@@ -31,6 +13,21 @@ class Bottles
     return "no more " if lower && bottles == 0
     "No more " if bottles == 0
   end
+
+  def container(quantity, second_line = false)
+    return quantity.to_s + " bottle" if quantity == 1
+    return "bottles" if quantity == -1 || quantity == 0
+    return "1 six-pack" if quantity == 6 && !second_line
+
+    quantity.to_s + " bottles"
+  end
+
+  def phrase(bottles)
+    return "Go to the store and buy some more," if bottles == 0
+    return "Take it down and pass it around," if bottles == 1
+    "Take one down and pass it around,"
+  end
+
 
   def verses(verse_start, verse_end)
     verse_start.downto(verse_end).map { |i| verse(i) }.join("\n")
