8712348e0a7b73b5de3c1719bd4a628f098cf001
Josh Young <j22cal@gmail.com>
2019-06-05 19:45:17 -0500

    Set Container to use attr_reader

--- lib/container.rb
+++ lib/container.rb
@@ -1,36 +1,38 @@
 class Container
 
-  def initialize(number)
-    @number = number
+  attr_reader :num
+
+  def initialize(num)
+    @num = num
   end
 
   def bottle
-    return "bottle" if @number == 1
+    return "bottle" if num == 1
 
     "bottles"
   end
 
   def number
-    return "no more" if @number == 0
+    return "no more" if num == 0
 
-    @number.to_s
+    num.to_s
   end
 
   def pronoun
-    return "it" if @number == 1
+    return "it" if num == 1
 
     "one"
   end
 
   def phrase
-    return "Go to the store and buy some more," if @number == 0
+    return "Go to the store and buy some more," if num == 0
 
     "Take #{pronoun} down and pass it around,"
   end
 
   def sequence
-    return "99" if @number == 0
+    return "99" if num == 0
 
-    @number - 1
+    num - 1
   end
 end
